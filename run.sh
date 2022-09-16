e352306232e163ad2f411888b194be6610aa30b8
coconan <coconan8@gmail.com>
2022-09-16 20:45:17 +0800

    support events directory

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/
 javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/Asset.java -d out
 javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/Account.java -d out
 javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/Application.java -d out
-java -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out me.coconan.agileaccount.Application ~/enduring-patience/investment/assets.md ~/enduring-patience/investment/events.md ~/enduring-patience/investment/funds.md
+java -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out me.coconan.agileaccount.Application ~/enduring-patience/investment/assets.md ~/enduring-patience/investment/events ~/enduring-patience/investment/funds.md
--- src/me/coconan/agileaccount/Application.java
+++ src/me/coconan/agileaccount/Application.java
@@ -1,6 +1,7 @@
 package me.coconan.agileaccount;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -12,10 +13,9 @@ public class Application {
     
     public static void main(String[] args) {
         Path assetsPath = Paths.get(args[0]);
-        Path operationsPath = Paths.get(args[1]);
+        Path operationsDirPath = Paths.get(args[1]);
         Path fundsPath = Paths.get(args[2]);
         try (FileReader assetsFileReader = new FileReader(assetsPath.toFile());
-            FileReader operationsFileReader = new FileReader(operationsPath.toFile());
             FileReader fundsFileReader = new FileReader(fundsPath.toFile())) {
             FundStore fundStore = new FundStore();
             try (BufferedReader reader = new BufferedReader(fundsFileReader)) {
@@ -30,14 +30,16 @@ public class Application {
             }
 
             Account account = new Account();
-            try (BufferedReader reader = new BufferedReader(operationsFileReader)) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] fields = line.split("\\s+");
-                    if (fields.length != 8) {
-                        continue;
+            for (File operationsFile : operationsDirPath.toFile().listFiles()) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(operationsFile))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        String[] fields = line.split("\\s+");
+                        if (fields.length != 8) {
+                            continue;
+                        }
+                        account.addOperation(new Operation(fundStore.get(fields[0].trim()), fields[3].trim(), fields[5].trim(), fields[4].trim(), fields[6].trim(), fields[7].trim()));
                     }
-                    account.addOperation(new Operation(fundStore.get(fields[0].trim()), fields[3].trim(), fields[5].trim(), fields[4].trim(), fields[6].trim(), fields[7].trim()));
                 }
             }
 
