37491ff52193c9fe45a88e4c8180707e3b4e8336
coconan <coconan.by@gmail.com>
2022-11-16 15:28:51 +0800

    integrate maven

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
-#!/bin/sh
-rm -rf out
-mkdir -p out
-javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/Fund.java -d out
-javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/FundStore.java -d out
-javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/Operation.java -d out
-javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/Asset.java -d out
-javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/Account.java -d out
-javac -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out src/me/coconan/agileaccount/Application.java -d out
-java -classpath {JAVA_8_HOME}/jre/lib/rt.jar:./out me.coconan.agileaccount.Application ~/enduring-patience/investment/assets.md ~/enduring-patience/investment/events ~/enduring-patience/investment/funds.md
+#!/bin/bash
+mvn clean install
+mvn package
+java -jar target/agile-account-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/enduring-patience/investment/assets.md ~/enduring-patience/investment/events ~/enduring-patience/investment/funds.md
