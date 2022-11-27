0205ff3bdca81ddb36eb691886272351c029207a
coconan <coconan8@gmail.com>
2022-11-27 20:52:57 +0800

    update version

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mvn clean install > /dev/null
 mvn package > /dev/null
-java -jar target/agile-account-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+java -jar target/agile-account-0.1.0-SNAPSHOT-jar-with-dependencies.jar \
     ~/enduring-patience/investment/assets.md \
     ~/enduring-patience/investment/events \
     ~/enduring-patience/investment/funds.md \
