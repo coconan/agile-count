894dd72df52b64e680fdd6208fecefc3ef21a218
coconan <coconan.by@gmail.com>
2024-05-03 16:58:12 +0800

    keep the code spotless

--- src/main/java/me/coconan/agileaccount/command/ChartCommand.java
+++ src/main/java/me/coconan/agileaccount/command/ChartCommand.java
@@ -1,14 +1,14 @@
+/* (C)2024 */
 package me.coconan.agileaccount.command;
-
-import me.coconan.agileaccount.Account;
-import me.coconan.agileaccount.ChartRow;
-import me.coconan.agileaccount.DateUtil;
-import me.coconan.agileaccount.InvestmentStats;
 
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import me.coconan.agileaccount.Account;
+import me.coconan.agileaccount.ChartRow;
+import me.coconan.agileaccount.DateUtil;
+import me.coconan.agileaccount.InvestmentStats;
 
 public class ChartCommand implements Command {
     private final String[] args;
@@ -33,24 +33,46 @@ public class ChartCommand implements Command {
             start = LocalDate.parse(args[4]);
             end = LocalDate.parse(args[5]);
         }
-        System.out.printf("%-10s %16s %16s %16s %16s %16s%% %16s %16s %16s %16s\n",
-                "date", "delta cost", "total cost", "net in/out", "total amount", "earning rate", "holding earning", "fixed earning", "accum earning", "daily earning");
+        System.out.printf(
+                "%-10s %16s %16s %16s %16s %16s%% %16s %16s %16s %16s\n",
+                "date",
+                "delta cost",
+                "total cost",
+                "net in/out",
+                "total amount",
+                "earning rate",
+                "holding earning",
+                "fixed earning",
+                "accum earning",
+                "daily earning");
         for (LocalDate date = start;
-            date.isBefore(DateUtil.changeStep(end, step, 1));
-            date = DateUtil.changeStep(date, step, 1)) {
+                date.isBefore(DateUtil.changeStep(end, step, 1));
+                date = DateUtil.changeStep(date, step, 1)) {
             ChartRow chartRow = ChartRow.build(date, step, investmentStatsByDate);
-            System.out.printf("%10s %16s %16s %16s %16s %16s%% %16s %16s %16s %16s\n",
-                dtf.format(chartRow.getDate()),
-                chartRow.getInvestmentThisDate().setScale(2, RoundingMode.HALF_DOWN),
-                chartRow.getAccumulatedInvestmentThisDate().setScale(2, RoundingMode.HALF_DOWN),
-                chartRow.getInvestmentThisDate().subtract(chartRow.getDeltaFixedEarnings()).setScale(2, RoundingMode.HALF_DOWN),
-                chartRow.getInvestmentStatsThisDate().getTotalAmount().setScale(2, RoundingMode.HALF_DOWN),
-                chartRow.getInvestmentStatsThisDate().getEarningRate().setScale(2, RoundingMode.HALF_DOWN),
-                chartRow.getInvestmentStatsThisDate().getTotalEarning().setScale(2, RoundingMode.HALF_DOWN),
-                chartRow.getInvestmentStatsThisDate().getTotalFixedEarning().setScale(2, RoundingMode.HALF_DOWN),
-                chartRow.getInvestmentStatsThisDate().getTotalAccumEarning().setScale(2, RoundingMode.HALF_DOWN),
-                chartRow.getDailyEarnings().setScale(2, RoundingMode.HALF_DOWN)
-            );
+            System.out.printf(
+                    "%10s %16s %16s %16s %16s %16s%% %16s %16s %16s %16s\n",
+                    dtf.format(chartRow.getDate()),
+                    chartRow.getInvestmentThisDate().setScale(2, RoundingMode.HALF_DOWN),
+                    chartRow.getAccumulatedInvestmentThisDate().setScale(2, RoundingMode.HALF_DOWN),
+                    chartRow.getInvestmentThisDate()
+                            .subtract(chartRow.getDeltaFixedEarnings())
+                            .setScale(2, RoundingMode.HALF_DOWN),
+                    chartRow.getInvestmentStatsThisDate()
+                            .getTotalAmount()
+                            .setScale(2, RoundingMode.HALF_DOWN),
+                    chartRow.getInvestmentStatsThisDate()
+                            .getEarningRate()
+                            .setScale(2, RoundingMode.HALF_DOWN),
+                    chartRow.getInvestmentStatsThisDate()
+                            .getTotalEarning()
+                            .setScale(2, RoundingMode.HALF_DOWN),
+                    chartRow.getInvestmentStatsThisDate()
+                            .getTotalFixedEarning()
+                            .setScale(2, RoundingMode.HALF_DOWN),
+                    chartRow.getInvestmentStatsThisDate()
+                            .getTotalAccumEarning()
+                            .setScale(2, RoundingMode.HALF_DOWN),
+                    chartRow.getDailyEarnings().setScale(2, RoundingMode.HALF_DOWN));
         }
     }
 }
